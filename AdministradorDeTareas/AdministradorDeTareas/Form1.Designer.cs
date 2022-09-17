8480aa3fe48e3c3c0742104acfad322d1f26eb5b
Jorgejl7 <jorgelopezen7@gmail.com>
2022-09-16 20:28:49 -0600

    Correccion menores de diseños

--- AdministradorDeTareas/AdministradorDeTareas/Form1.Designer.cs
+++ AdministradorDeTareas/AdministradorDeTareas/Form1.Designer.cs
@@ -29,24 +29,211 @@ namespace AdministradorDeTareas
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtContador = new System.Windows.Forms.Label();
+            this.txtNombreProceso = new System.Windows.Forms.Label();
+            this.txtProceso = new System.Windows.Forms.Label();
+            this.dgvAdministrador = new MetroFramework.Controls.MetroGrid();
+            this.dgvProceso = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvFisica = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvVirtual = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvCPU = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAdministrador)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AccessibleName = "btnActualizar";
+            this.button1.Location = new System.Drawing.Point(36, 521);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Actulizar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.AccessibleName = "btnDetener";
+            this.button2.Location = new System.Drawing.Point(235, 521);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Detener";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.AccessibleName = "btnSalir";
+            this.button3.Location = new System.Drawing.Point(710, 521);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(132, 29);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Salir";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // txtContador
+            // 
+            this.txtContador.AutoSize = true;
+            this.txtContador.Location = new System.Drawing.Point(36, 560);
+            this.txtContador.Name = "txtContador";
+            this.txtContador.Size = new System.Drawing.Size(50, 20);
+            this.txtContador.TabIndex = 3;
+            this.txtContador.Text = "label1";
+            // 
+            // txtNombreProceso
+            // 
+            this.txtNombreProceso.AutoSize = true;
+            this.txtNombreProceso.Location = new System.Drawing.Point(568, 560);
+            this.txtNombreProceso.Name = "txtNombreProceso";
+            this.txtNombreProceso.Size = new System.Drawing.Size(143, 20);
+            this.txtNombreProceso.TabIndex = 4;
+            this.txtNombreProceso.Text = "Nombre De Proceso";
+            // 
+            // txtProceso
+            // 
+            this.txtProceso.AutoSize = true;
+            this.txtProceso.Location = new System.Drawing.Point(718, 561);
+            this.txtProceso.Name = "txtProceso";
+            this.txtProceso.Size = new System.Drawing.Size(15, 20);
+            this.txtProceso.TabIndex = 5;
+            this.txtProceso.Text = "-";
+            // 
+            // dgvAdministrador
+            // 
+            this.dgvAdministrador.AccessibleName = "dgvAdministrador";
+            this.dgvAdministrador.AllowUserToResizeRows = false;
+            this.dgvAdministrador.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dgvAdministrador.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvAdministrador.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dgvAdministrador.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvAdministrador.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvAdministrador.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAdministrador.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgvProceso,
+            this.dgvID,
+            this.dgvFisica,
+            this.dgvVirtual,
+            this.dgvCPU});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvAdministrador.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvAdministrador.EnableHeadersVisualStyles = false;
+            this.dgvAdministrador.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.dgvAdministrador.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dgvAdministrador.Location = new System.Drawing.Point(47, 91);
+            this.dgvAdministrador.Name = "dgvAdministrador";
+            this.dgvAdministrador.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvAdministrador.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvAdministrador.RowHeadersWidth = 51;
+            this.dgvAdministrador.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dgvAdministrador.RowTemplate.Height = 29;
+            this.dgvAdministrador.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvAdministrador.Size = new System.Drawing.Size(822, 405);
+            this.dgvAdministrador.TabIndex = 6;
+            // 
+            // dgvProceso
+            // 
+            this.dgvProceso.HeaderText = "Proceso";
+            this.dgvProceso.MinimumWidth = 6;
+            this.dgvProceso.Name = "dgvProceso";
+            this.dgvProceso.ReadOnly = true;
+            this.dgvProceso.Width = 125;
+            // 
+            // dgvID
+            // 
+            this.dgvID.HeaderText = "ID";
+            this.dgvID.MinimumWidth = 6;
+            this.dgvID.Name = "dgvID";
+            this.dgvID.ReadOnly = true;
+            this.dgvID.Width = 125;
+            // 
+            // dgvFisica
+            // 
+            this.dgvFisica.HeaderText = "Memoria Fisica";
+            this.dgvFisica.MinimumWidth = 6;
+            this.dgvFisica.Name = "dgvFisica";
+            this.dgvFisica.ReadOnly = true;
+            this.dgvFisica.Width = 125;
+            // 
+            // dgvVirtual
+            // 
+            this.dgvVirtual.HeaderText = "Memoria Virtual";
+            this.dgvVirtual.MinimumWidth = 6;
+            this.dgvVirtual.Name = "dgvVirtual";
+            this.dgvVirtual.ReadOnly = true;
+            this.dgvVirtual.Width = 125;
+            // 
+            // dgvCPU
+            // 
+            this.dgvCPU.HeaderText = "CPU";
+            this.dgvCPU.MinimumWidth = 6;
+            this.dgvCPU.Name = "dgvCPU";
+            this.dgvCPU.ReadOnly = true;
+            this.dgvCPU.Width = 125;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(914, 600);
+            this.Controls.Add(this.dgvAdministrador);
+            this.Controls.Add(this.txtProceso);
+            this.Controls.Add(this.txtNombreProceso);
+            this.Controls.Add(this.txtContador);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Administrador de Tareas";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAdministrador)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label txtContador;
+        private System.Windows.Forms.Label txtNombreProceso;
+        private System.Windows.Forms.Label txtProceso;
+        private MetroFramework.Controls.MetroGrid dgvAdministrador;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvProceso;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvFisica;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvVirtual;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvCPU;
     }
 }
 
