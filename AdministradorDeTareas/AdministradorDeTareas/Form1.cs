927bffc8d167a1d7e9a02b5c9f37129195f2f365
Jorgejl7 <jorgelopezen7@gmail.com>
2022-09-16 20:42:16 -0600

    creacion de la clase UpdateProccessList

--- AdministradorDeTareas/AdministradorDeTareas/Form1.cs
+++ AdministradorDeTareas/AdministradorDeTareas/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,7 +17,21 @@ namespace AdministradorDeTareas
         {
             InitializeComponent();
         }
-
+        private void UpdateProcessList()
+        {
+            dgvAdministrador.Rows.Clear();
+            foreach (Process p in Process.GetProcesses())
+            {
+                //Aqui se llenan los datos del sistema en el data grid
+                int n = dgvAdministrador.Rows.Add();
+                dgvAdministrador.Rows[n].Cells[0].Value = p.ProcessName;
+                dgvAdministrador.Rows[n].Cells[1].Value = p.Id;
+                dgvAdministrador.Rows[n].Cells[2].Value = DarFormato((p.WorkingSet64 / 1024).ToString());
+                dgvAdministrador.Rows[n].Cells[3].Value = DarFormato((p.VirtualMemorySize64 / 1024).ToString());
+                dgvAdministrador.Rows[n].Cells[4].Value = p.SessionId;
+            }
+            txtContador.Text = "Procesos Actuales: " + dgvAdministrador.Rows.Count.ToString();
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
 
